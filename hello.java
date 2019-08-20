03dcedaaf585aeafdc45c6fc0b0538d715163e27
Administrator@FRANCISSAMRI.COM <Administrator@FRANCISSAMRI.COM>
2019-08-19 20:19:35 -0500

    second commit

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello
 {
 public static void main (string[], args);
 {
- //Prints hello, world on windows console
-System.Out.println("Hello, World);
+ //Prints hello, world on windowsconsole 
+System.Out.println("Hello, World");
 }
 }
