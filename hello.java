99175fe024e6bf3be26f507d0135a51976a8412c
Administrator@FRANCISSAMRI.COM <Administrator@FRANCISSAMRI.COM>
2019-08-19 20:23:45 -0500

    third commit

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello
 {
-public static void main (string[], args);
+public static void main(string[], args);
 {
  //Prints hello, world on windowsconsole 
 System.Out.println("Hello, World");
